91d14b9ee571e0251e0da245fa18dd91a548ce17
09936-eng <09936@khamphoempittaya.ac.th>
2026-09-16 23:04:31 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,94 +1,26 @@
-const CACHE_NAME =
-  "dream-music-v2";
+const CACHE_NAME = "dream-music-v2";
 
 const FILES = [
-
-"./",
+  "./",
   "./index.html",
   "./css/style.css",
   "./js/app.js",
   "./manifest.json",
   "./song.mp3"
-
 ];
 
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES);
+    })
+  );
+});
 
-self.addEventListener(
-  "install",
-  event => {
-
-    event.waitUntil(
-
-      caches.open(
-        CACHE_NAME
-      )
-      .then(cache => {
-
-        return cache.addAll(
-          FILES
-        );
-
-      })
-
-    );
-
-  }
-);
-
-
-self.addEventListener(
-  "activate",
-  event => {
-
-    event.waitUntil(
-
-      caches.keys()
-        .then(keys => {
-
-          return Promise.all(
-
-            keys
-              .filter(
-                key =>
-                  key !==
-                  CACHE_NAME
-              )
-              .map(
-                key =>
-                  caches.delete(
-                    key
-                  )
-              )
-
-          );
-
-        })
-
-    );
-
-  }
-);
-
-
-self.addEventListener(
-  "fetch",
-  event => {
-
-    event.respondWith(
-
-      caches.match(
-        event.request
-      )
-      .then(cached => {
-
-        return (
-          cached ||
-          fetch(event.request)
-        );
-
-      })
-
-    );
-
-  }
-);
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
